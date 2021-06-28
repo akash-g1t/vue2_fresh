4ebb4d452c9730ceb0a74dab592689963b190faf
Akash Chauhan <developer@account.com>
2021-06-28 22:02:20 +0530

    committed with ssh

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p>This is a TodoList app. First App Built with Vue.js</p>
+    <p>This is a TodoList app. First App Built with Vue.js.</p>
   </div>
 </template>
